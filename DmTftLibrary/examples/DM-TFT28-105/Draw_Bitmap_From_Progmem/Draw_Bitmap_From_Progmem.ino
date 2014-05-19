3e439da36ea5af79d964e5de7d24446088b5e482
Tomas Köhn <tomas.kohn@gmail.com>
2014-05-19 16:01:53 +0800

    Added example to use show image from Progmem

---
+++ DmTftLibrary/examples/DM-TFT28-105/Draw_Bitmap_From_Progmem/Draw_Bitmap_From_Progmem.ino
@@ -0,0 +1,58 @@
+/**********************************************************************************************
+ Copyright (c) 2014 DisplayModule. All rights reserved.
+
+ Redistribution and use of this source code, part of this source code or any compiled binary
+ based on this source code is permitted as long as the above copyright notice and following
+ disclaimer is retained. 
+
+ DISCLAIMER:
+ THIS SOFTWARE IS SUPPLIED "AS IS" WITHOUT ANY WARRANTIES AND SUPPORT. DISPLAYMODULE ASSUMES
+ NO RESPONSIBILITY OR LIABILITY FOR THE USE OF THE SOFTWARE.
+ ********************************************************************************************/
+#include <SPI.h>
+#include <SPIFlash.h>
+#include <SD.h>
+#include <DmTftIli9341.h>
+#include <DmDrawBmpFromProgmem.h>
+
+#define TFT_CS  10
+#define SD_CS   8
+#define F_CS    6
+#define T_CS    4
+
+DmTftIli9341 tft = DmTftIli9341(10, 9);
+DmDrawBmpFromProgmem drawImage = DmDrawBmpFromProgmem();
+
+extern uint8_t dl_h565_t2[];
+
+void setup()
+{
+  // Set CS SPI pin HIGH for all SPI units, so they don't interfere
+  pinMode(TFT_CS, OUTPUT);
+  pinMode(T_CS, OUTPUT);
+  pinMode(SD_CS, OUTPUT);
+  pinMode(F_CS, OUTPUT);
+  digitalWrite(TFT_CS, HIGH);
+  digitalWrite(T_CS, HIGH);
+  digitalWrite(SD_CS, HIGH);
+  digitalWrite(F_CS, HIGH);
+  
+  Serial.begin(9600);
+  tft.init();
+   
+  if (! drawImage.printHeaderInfo(dl_h565_t2)) {
+    Serial.println(F("Can not open bitmap."));
+    return;
+  }
+  
+  uint32_t startTime, endTime;
+  
+  startTime = millis();
+  drawImage.drawImage(dl_h565_t2, tft, 10, 0);
+  endTime = millis();
+  Serial.print("Draw Image: ");
+  Serial.print(endTime - startTime, DEC);
+  Serial.println(" ms");
+}
+
+void loop() { }
